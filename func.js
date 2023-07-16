57c94e939e6f672c17059360f2c6efa89d3cc383
MUGISHA Fred <fmugisha10@gmail.com>
2023-07-16 10:48:23 +0200

    Add UI to Rock Paper Scissors Game

--- func.js
+++ func.js
@@ -1,25 +1,78 @@
+game();
 function game() {
-    const player = prompt("Select your choice!");
-    const playerSelection = player.toUpperCase();
-    const computerSelection = getComputerChoice();
+
+    const computerChoice = document.getElementById("comp");
+    const playerChoice = document.getElementById("play");
+    const result = document.getElementById("res");
+    const playerSelected = document.querySelectorAll(".game-cta")
+    const computerScoreText = document.getElementById("compScore");
+    const playerScoreText = document.getElementById("playScore");
+
+    let computerSelection;
+    let playerSelection;
+    let computerScore = 0;
+    let playerScore = 0;
+
+    playerSelected.forEach(button => button.addEventListener('click', () => {
+        const player = button.textContent;
+        playerSelection = player.toUpperCase();
+        getComputerChoice();
+        computerChoice.textContent = `${computerSelection}`;
+        playerChoice.textContent = `${playerSelection}`;
+
+        const final = playRound(playerSelection, computerSelection);
+        if (final === "Draw") {
+            computerScore += 0;
+            playerScore += 0;
+        } else if (final === "You Win") {
+            playerScore += 1;
+        } else {
+            computerScore += 1;
+        }
+
+        computerScoreText.textContent = `${computerScore}`;
+        playerScoreText.textContent = `${playerScore}`;
+        result.textContent = `${playRound(playerSelection, computerSelection)}`;
+        
+        console.log("Player Selection: " + playerSelection);
+        console.log("Computer Selection: " + computerSelection);
+        console.log("Results: " + playRound(playerSelection, computerSelection));
+        console.log("");
+
+        if (playerScore === 5) {
+            if (playerScore === computerScore) {
+                result.textContent = `You got same scores with Computer!`;
+            } else if (playerScore > computerScore) {
+                result.textContent = `You win with ${playerScore} scores`;
+            } else {
+                result.textContent = `Computer win with ${computerScore} scores`;
+            };
+
+            gameOver();
+        }
+    }));
+
+    function gameOver() {
+        playerScore = 0;
+        computerScore = 0; 
+    }
 
     function getComputerChoice() {
-        let computer;
         const randomNumber = Math.floor(Math.random() * 3) + 1;
 
         switch(randomNumber) {
             case 1:
-                computer = "ROCK";
+                computerSelection = "ROCK";
                 break;
             case 2:
-                computer = "PAPER";
+                computerSelection = "PAPER";
                 break;
             case 3:
-                computer = "SCISSORS";
+                computerSelection = "SCISSORS";
                 break;
         }
 
-        return computer;
+        return computerSelection;
     }
 
     function playRound(playerSelection, computerSelection) {
@@ -33,29 +86,4 @@ function game() {
             return (playerSelection === "ROCK") ? "You Win" : "You Lose! Scissors beats Paper";
         }
     }
-
-    console.log("Player Selection: " + playerSelection);
-    console.log("Computer Selection: " + computerSelection);
-    console.log("Results: " + playRound(playerSelection, computerSelection));
-    console.log("");
-
-    return playRound(playerSelection, computerSelection);
-
 }
-
-let computerScore = 0;
-let playerScore = 0;
-for (let i=0; i<5; i++) {
-    const final = game();
-    if (final === "Draw") {
-        computerScore += 0;
-        playerScore += 0;
-    } else if (final === "You Win") {
-        playerScore += 20;
-    } else {
-        computerScore += 20;
-    }
-}
-
-console.log("Computer Score: " + computerScore + "%");
-console.log("Player Score: " + playerScore + "%");
